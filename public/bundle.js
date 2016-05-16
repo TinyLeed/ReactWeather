57ccba5813aa2fc09296cbd4644a567f47ecc9a2
TinyLeed <mads.p@icloud.com>
2016-05-17 00:33:58 +0200

    Rename about page and add paragraph text

--- public/bundle.js
+++ public/bundle.js
@@ -26172,9 +26172,18 @@
 
 	var About = function About(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'About Component'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'About'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Welcome to the about page.'
+	        )
 	    );
 	};
 
